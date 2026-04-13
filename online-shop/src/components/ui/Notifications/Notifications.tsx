13af0c1408417979cbf051a7f1f91d1789772a1b
AminKei <moakeimasi@gmail.com>
2026-04-14 00:32:42 +0330

    fix bugs

--- online-shop/src/components/ui/Notifications/Notifications.tsx
+++ online-shop/src/components/ui/Notifications/Notifications.tsx
@@ -11,7 +11,7 @@ export default function Notifications() {
 
   return (
     <Drawer
-      placement="bottom"
+      placement=""
       height={220}
       open={open}
       onClose={() => setOpen(false)}
